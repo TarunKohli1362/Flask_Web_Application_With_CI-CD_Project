fd05a89f22c5b9b9a35a54b808e42d1a9e71051d
Tarun Kohli <tarunkohli1362@gmail.com>
2025-07-25 18:12:31 +0530

    3rd

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask 
+
+app = Flask(_name_)
+
+@app.route("/info")
+def lwinfo():
+	return "I Am LW From India"
+
+@app.route("/name")
+def lwname():
+	return "My Name Is TARUN KOHLI"
+
+@app.route("/email")
+def lwemail():
+	return "My Email ID Is tarunkohli5555@gmail.com"
+
+@app.route("/phone")
+def lwphone():
+	return "My Phone Number Is 7404650169"
+
+app.run(host="0.0.0.0"
